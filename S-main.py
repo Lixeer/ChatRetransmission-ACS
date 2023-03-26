aa8e97c5a633e38b97ebe943d35c5ad838c5cb86
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-03-26 09:12:30 +0800

    Update S-main.py

--- S-main.py
+++ S-main.py
@@ -1,24 +1,32 @@
-from flask import Flask, jsonify, request
+import aiohttp
+import asyncio
+from aiohttp import web
 
-app = Flask(__name__)
+openai.openai.api_key = "这里填入你的KEY"
 
-import openai
-openai.api_key="这里填入你的KEY"
-def chat(msg:list):
-    rp=openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=msg
-    )
-    return rp
+async def chat(msg: list) -> dict:
+    async with aiohttp.ClientSession() as session:
+        url = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+        headers = {"Authorization": f"Bearer {openai.openai.api_key}"}
+        data = {
+            "prompt": f"{msg}",
+            "max_tokens": 60,
+            "temperature": 0.5,
+            "n": 1,
+            "stop": ""
+        }
+        async with session.post(url, headers=headers, json=data) as resp:
+            response = await resp.json()
+            return response["choices"][0]["text"]
 
+async def process_dict(request):
+    data = await request.json()
+    msg = data['msg']
+    response = await chat(msg)
+    return web.Response(text=response)
 
-
-
-@app.route('/chat', methods=['POST'])
-def process_dict():
-    data = request.get_json()
-    rp=chat(data['msg'])
-    return rp
+app = web.Application()
+app.add_routes([web.post('/chat', process_dict)])
 
 if __name__ == '__main__':
-    app.run(port=5701,host="0.0.0.0")
+    web.run_app(app, port=5701, host="0.0.0.0")
