bbe25312aaccf02debb2d988fb7bc8a11d0df791
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-03-26 08:50:40 +0800

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import requests
 import json
 def userMsgCreat(msg:str) -> dict:
     '''
-    msg:用于构造用户回复内容
+    :param msg:用于构造用户回复内容
     '''
     #用户信息工厂
     return {"role": "user", "content": msg}
@@ -12,14 +12,14 @@ def userMsgCreat(msg:str) -> dict:
 
 def aiMsgCreat(msg:str) -> dict:
     '''
-    msg:用于构造ai回复内容
+    :param msg:用于构造ai回复内容
     '''
     #ai信息工厂
     return {"role": "assistant", "content": msg}
 
 def get_chat(msg:list) -> str:
     """
-    msg：纪录上下文信息
+    :param msg：纪录上下文信息
     """
     url = "http://url/chat2"  #url填入服务端域名或者公网IP
 
