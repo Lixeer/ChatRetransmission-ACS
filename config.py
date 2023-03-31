bda5072ef9d275e92c186a9b71bf491c84e072b3
Spanx <89501533+Lixeer@users.noreply.github.com>
2023-03-31 18:01:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-
-VERSION = (1,0,0)
-IP = "0.0.0.0"
-PORT = "5701"
+VERSION = (1,2,0)
+IP = "0.0.0.0"   #可选127.0.0.1和0.0.0.0 前者只供本机访问 后者供全局访问
+PORT = 5702
 COLD_TIME = 30
-
+KEY="sk-xxxxxxxxxxxxxxxxxxxx"
 class Appconfing():
     pass
